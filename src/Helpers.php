957abb6ca2feafc12b43a8751ec22bd2e7fa28ea
Seb Kay <seb@fhoke.com>
2024-06-18 14:52:53 +0100

    Update text domain

--- src/Helpers.php
+++ src/Helpers.php
@@ -9,47 +9,47 @@ class Helpers
         return [
             [
                 'slug' => 'five_minutes',
-                'label' => \__('5 minutes', 'paris-tile-stone-profits-connector'),
+                'label' => \__('5 minutes', 'wp-cronable'),
                 'value' => 300,
             ],
             [
                 'slug' => 'ten_minutes',
-                'label' => \__('10 minutes', 'paris-tile-stone-profits-connector'),
+                'label' => \__('10 minutes', 'wp-cronable'),
                 'value' => 600,
             ],
             [
                 'slug' => 'fifteen_minutes',
-                'label' => \__('15 minutes', 'paris-tile-stone-profits-connector'),
+                'label' => \__('15 minutes', 'wp-cronable'),
                 'value' => 900,
             ],
             [
                 'slug' => 'twenty_minutes',
-                'label' => \__('20 minutes', 'paris-tile-stone-profits-connector'),
+                'label' => \__('20 minutes', 'wp-cronable'),
                 'value' => 1200,
             ],
             [
                 'slug' => 'thirty_minutes',
-                'label' => \__('30 minutes', 'paris-tile-stone-profits-connector'),
+                'label' => \__('30 minutes', 'wp-cronable'),
                 'value' => 1800,
             ],
             [
                 'slug' => 'forty_five_minutes',
-                'label' => \__('45 minutes', 'paris-tile-stone-profits-connector'),
+                'label' => \__('45 minutes', 'wp-cronable'),
                 'value' => 2700,
             ],
             [
                 'slug' => 'one_hour',
-                'label' => \__('1 hour', 'paris-tile-stone-profits-connector'),
+                'label' => \__('1 hour', 'wp-cronable'),
                 'value' => 3600,
             ],
             [
                 'slug' => 'four_hours',
-                'label' => \__('4 hours', 'paris-tile-stone-profits-connector'),
+                'label' => \__('4 hours', 'wp-cronable'),
                 'value' => 14400,
             ],
             [
                 'slug' => 'daily',
-                'label' => \__('1 day', 'paris-tile-stone-profits-connector'),
+                'label' => \__('1 day', 'wp-cronable'),
                 'value' => 86400,
             ],
         ];
